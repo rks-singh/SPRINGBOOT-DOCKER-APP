3b03bf5c8c80f372073d9788f1acb450d4ef4938
RAVI SHEKHAR SINGH <72653726+rks-singh@users.noreply.github.com>
2023-11-23 01:26:48 +0530

    Update MessageController.java

--- src/main/java/com/ravi/restcontroller/MessageController.java
+++ src/main/java/com/ravi/restcontroller/MessageController.java
@@ -8,6 +8,6 @@ public class MessageController {
 
 	@GetMapping("/msg")
 	public String getMessage() {
-		return "WELCOME TO SPRINGBOOT DOCKER APP.\n Here i am performing GIT + MAVEN + JENKINS + DOCKER INTERIGATION\n CI/CD";
+		return "WELCOME TO SPRINGBOOT DOCKER APP.\n Here i am performing GIT + MAVEN + JENKINS + DOCKER INTERIGATION\n CI/CD (INDIA)";
 	}
 }
